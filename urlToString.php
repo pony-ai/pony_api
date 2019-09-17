b372836e55a611d6744fa1049e36d988149ecce0
ponyxiao <ponyxiao@xiaoe-tech.com>
2019-09-17 18:34:39 +0800

    'xiaoqtc.cn'

--- urlToString.php
+++ urlToString.php
@@ -1,17 +1,10 @@
 <?php
-
 $str = 'http://appwXZviG734747.h5.inside.xiaoeknow.com/content_page/eyJ0eXBlIjozLCJyZXNvdXJjZV90eXBlIjoiIiwicmVzb3VyY2VfaWQiOiIiLCJwcm9kdWN0X2lkIjoicF81ZDMxOGY4MzcyNjZhX2VDQVloODVKIiwiYXBwX2lkIjoiYXBwd1hadmlHNzM0NzQ3In0';
-
 $str = substr($str,strrpos($str, '/')+1);
-
 $str = base64_decode($str);
-
 $arr = ['pay_info' => $str];
-
 $re = http_build_query($arr);
-
 echo $re . "\n";
-
 /*
 pay_info=%7B%22type%22%3A3%2C%22resource_type%22%3A%22%22%2C%22resource_id%22%3A
 %22%22%2C%22product_id%22%3A%22p_5d318f837266a_eCAYh85J%22%2C%22app_id%22%3A%22a
