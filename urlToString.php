49cb8340c734c2b4532348045588fecf4b0a2e65
ponyxiao <ponyxiao@xiaoe-tech.com>
2019-09-18 10:00:13 +0800

    'update'

--- urlToString.php
+++ urlToString.php
@@ -1,11 +1,10 @@
 <?php
-$str = 'http://appwXZviG734747.h5.inside.xiaoeknow.com/content_page/eyJ0eXBlIjozLCJyZXNvdXJjZV90eXBlIjoiIiwicmVzb3VyY2VfaWQiOiIiLCJwcm9kdWN0X2lkIjoicF81ZDMxOGY4MzcyNjZhX2VDQVloODVKIiwiYXBwX2lkIjoiYXBwd1hadmlHNzM0NzQ3In0';
+$str = 'https://wx4459d36c099a5f7b.h5.test.xiaoe-tech.com/mp/eyJpZCI6MzA2NTM5fQ';
 $str = substr($str,strrpos($str, '/')+1);
 $str = base64_decode($str);
 $arr = ['param' => $str];
 $re = http_build_query($arr);
 echo $re . "\n";
-
 /*
  * 打印结果
 pay_info=%7B%22type%22%3A3%2C%22resource_type%22%3A%22%22%2C%22resource_id%22%3A
